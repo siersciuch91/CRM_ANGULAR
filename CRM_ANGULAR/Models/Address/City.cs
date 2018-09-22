212a902d9f75d8b9468da8798e396f3bfd20fc2a
Adrian Grzegorek <siersciuch1991@gmail.com>
2018-09-22 18:59:40 +0200

    Add connection to database and add nuget packages

---
+++ CRM_ANGULAR/Models/Address/City.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CRM_ANGULAR.Models.Address
+{
+  [Table("City")]
+  public class City
+  {
+    public int Id { get; set; }
+
+    [Required]
+    [StringLength(50)]
+    public string Name { get; set;}
+  }
+}
